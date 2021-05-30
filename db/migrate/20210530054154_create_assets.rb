2f4389b355b91e455610e96791c30618a8c5d988
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-30 14:44:38 +0900

    feat: 🎸 $ bundle exec rails db:migrate

--- db/migrate/20210530054154_create_assets.rb
+++ db/migrate/20210530054154_create_assets.rb
@@ -1,6 +1,11 @@
 class CreateAssets < ActiveRecord::Migration[6.1]
   def change
     create_table :assets do |t|
+      t.bigint :id_number
+      t.string :url
+      t.string :asset_type
+
+      t.references :tweet
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_053438) do
+ActiveRecord::Schema.define(version: 2021_05_30_054154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assets", force: :cascade do |t|
+    t.bigint "id_number"
+    t.string "url"
+    t.string "asset_type"
+    t.bigint "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_assets_on_tweet_id"
+  end
 
   create_table "hashtags", force: :cascade do |t|
     t.string "text"
