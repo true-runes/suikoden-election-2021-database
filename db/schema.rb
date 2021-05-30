26351a326335e2969685f4e842922a5335d893df
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-30 17:09:36 +0900

    feat: 🎸 $ bin/rails g paper_trail:install && db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_054844) do
+ActiveRecord::Schema.define(version: 2021_05_30_080905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,17 @@ ActiveRecord::Schema.define(version: 2021_05_30_054844) do
     t.index ["id_number"], name: "index_users_on_id_number", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["screen_name"], name: "index_users_on_screen_name"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type"
+    t.string "{:null=>false}"
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
