6a0fdf46f6857d3c8d3f703e3472d73362190c5d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-30 17:52:52 +0900

    feat: 🎸 MentionedUser にカラムを設定した

--- db/migrate/20210530085016_create_mentioned_users.rb
+++ db/migrate/20210530085016_create_mentioned_users.rb
@@ -1,6 +1,9 @@
 class CreateMentionedUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :mentioned_users do |t|
+      t.bigint :id_number
+
+      t.references :tweet
 
       t.timestamps
     end
