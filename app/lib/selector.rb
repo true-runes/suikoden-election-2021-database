bbefb470cc2b55a0b32f4f55c06c94eb1e5773bd
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-31 10:26:51 +0900

    Selector と Tweet をリファクタリング (#60)
    * refactor: 💡 scope の命名をより適切なものに変更した
    
    * refactor: 💡 Selector のメソッドを使いやすいものに修正

--- app/lib/selector.rb
+++ app/lib/selector.rb
@@ -1,9 +1,13 @@
 class Selector
   class << self
-    def newer_tweets_by_hashtag_search_from_tweet_storage(hashtag)
-      maximum_tweet_id_number = Tweet.with_hashtag(hashtag).maximum(:id_number)
-
-      BySearchWordTweet.where(search_word: "##{hashtag}").where('id_number > ?', maximum_tweet_id_number)
+    # 対象を絞って高速化する
+    # 'hashtag' には '#' は不要
+    def excluded_already_existing_tweets_by_hashtag_from_tweet_storage(hashtag)
+      BySearchWordTweet.where(
+        search_word: "##{hashtag}"
+      ).where.not(
+        id_number: Tweet.contains_hashtag(hashtag).pluck(:id_number)
+      )
     end
   end
 end
