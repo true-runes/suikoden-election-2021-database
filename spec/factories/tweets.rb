ad66389ffb245ae9effc9b042da2909930c51a48
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-05-31 10:15:36 +0900

    test: 💍 Tweet と User のファクトリを追加

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :tweet do
-    
+    id_number { 192789663528910849 }
+    full_text { "おはようございます！\nこんにちは！" }
+    source { '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>' }
+    in_reply_to_tweet_id_number { nil }
+    in_reply_to_user_id_number { nil }
+    is_retweet { false }
+    language { 'ja' }
+    is_public { true }
+    tweeted_at { Time.zone.now.yesterday }
   end
 end
