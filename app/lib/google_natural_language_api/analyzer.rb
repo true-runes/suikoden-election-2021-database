bcb042c3e47993d363065d5adae333610c1b4945
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-06-11 14:40:47 +0900

    feat: 🎸 DirectMessage を AnalyzeSyntax に取り込むインポータを作成 (#112)

--- app/lib/google_natural_language_api/analyzer.rb
+++ app/lib/google_natural_language_api/analyzer.rb
@@ -14,7 +14,11 @@ module GoogleNaturalLanguageApi
 
       ActiveRecord::Base.transaction do
         Tweet.not_retweet.each do |tweet|
-          analyzer.create_analyze_syntax_record(tweet) if AnalyzeSyntax.find_by(tweet_id: tweet.id).blank?
+          analyzer.create_analyze_syntax_record(tweet, category: 'tweet') if AnalyzeSyntax.find_by(tweet_id: tweet.id).blank?
+        end
+
+        DirectMessage.all.each do |direct_message|
+          analyzer.create_analyze_syntax_record(direct_message, category: 'direct_message') if AnalyzeSyntax.find_by(direct_message_id: direct_message.id).blank?
         end
       end
     end
@@ -24,23 +28,26 @@ module GoogleNaturalLanguageApi
       @client = Google::Cloud::Language.language_service
     end
 
-    def create_analyze_syntax_record(tweet)
-      response = analyze_tweet_syntax_by_api(tweet)
+    def create_analyze_syntax_record(tweet_or_dm, category: nil)
+      return unless category.in?(%w(tweet direct_message))
+
+      response = analyze_tweet_syntax_by_api(tweet_or_dm, category: category)
 
       ActiveRecord::Base.transaction do
         analyze_syntax = AnalyzeSyntax.new(
           language: response.language,
           sentences: response.sentences.map(&:to_json),
           tokens: response.tokens.map(&:to_json),
-          tweet_id: tweet.id
+          tweet_id: category == 'tweet' ? tweet_or_dm.id : nil,
+          direct_message_id: category == 'direct_message' ? tweet_or_dm.id : nil
         )
 
         analyze_syntax.save!
       end
     end
 
-    def analyze_tweet_syntax_by_api(tweet)
-      content = tweet.full_text
+    def analyze_tweet_syntax_by_api(tweet_or_dm, category: nil)
+      content = category == 'tweet' ? tweet_or_dm.full_text : tweet_or_dm.text
       document = { type: :PLAIN_TEXT, content: content }
 
       @language.analyze_syntax(document: document)
